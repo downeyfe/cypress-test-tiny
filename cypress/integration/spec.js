1e32862310a8fc3a9256c1812832bdb1a85d3efe
Frances Downey <frances.downey@bookatable.com>
2018-11-26 16:44:09 +0000

    Add test to show NaN duration output

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,6 @@
-describe('page', () => {
-  it('works', () => {})
-})
+describe('Describe Test', () => {
+  it('renders page', () => {
+    cy.visit('www.google.com')
+    cy.title().should('equal', 'Google');
+  });
+});
